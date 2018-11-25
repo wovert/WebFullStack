313d96413b38f05e36febaebb0820499f787aba5
wovert <wovert@126.com>
2018-11-25 10:47:53 +0800

    add cluster

--- node/demo03/cluster.js
+++ node/demo03/cluster.js
@@ -3,15 +3,18 @@ const cluster=require('cluster');
 const os=require('os');
 const process=require('process');
 
+// 是否为主进程
 if(cluster.isMaster){
   for(let i=0;i<os.cpus().length;i++){
-    cluster.fork();
+    cluster.fork(); // 创建子进程
   }
 
   console.log('cpu核心数：', os.cpus().length);
   console.log('主进程');
 }else{
   let server=http.createServer((req, res)=>{
+    
+    // 进程IP
     console.log(process.pid);
 
     res.write('aaaa');
