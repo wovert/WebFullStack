192ba1d6aa0cb83098a33c18fb219cbc53fca8c8
wovert <67668283@qq.com>
2019-06-17 10:47:46 +0800

    add Symbol datatype

--- es6/symbol/symbol.js
+++ es6/symbol/symbol.js
@@ -17,3 +17,21 @@ console.log(obj[name]) // hello
 
 console.log(Object.getOwnPropertySymbols(obj)) // 返回所有属性名是 Symbol 类型的数组
 console.log(Object.keys(obj)) // ['a']
+
+var Person = (function() {
+  let _gender = Symbol('gender')
+
+  function p(name, gender) {
+    this.name = name
+    this[_gender] = gender
+  }
+  p.prototype.say = function() {
+    console.log(this[_gender])
+  }
+  return p
+})()
+
+let p1 = new Person('wovert', '男')
+console.log(p1)
+p1.say()
+console.log(p1[Symbol('gender')]) // undefined
