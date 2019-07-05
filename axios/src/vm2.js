0b06e24eda8be0613b54e34132bd68e408b37ba6
wovert <67668283@qq.com>
2019-07-05 15:05:44 +0800

    axios

---
+++ axios/src/vm2.js
@@ -0,0 +1,32 @@
+import Vue from 'vue/dist/vue.esm';
+import Axios from 'axios';
+import {stringify} from 'querystring';
+
+const axios=Axios.create({
+  transformRequest: [
+    function (data){
+      return stringify(data);
+    }
+  ]
+});
+
+let vm=new Vue({
+  el: '#div1',
+  data: {
+    result: 0
+  },
+  async created(){
+    let {data}=await axios({
+      url: './data/sum.php',
+      method: 'post',
+      data: {a: 55, b: 99}
+    });
+
+    this.result=data;
+  },
+  template: `
+  <div>
+    {{result}}
+  </div>
+  `
+})
