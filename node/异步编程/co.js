bddb219e54a5889f7f466e38b4d6733f2c0f5d0f
wovert <67668283@qq.com>
2019-07-11 10:24:03 +0800

    ads vscode setting file

--- node/异步编程/co.js
+++ node/异步编程/co.js
@@ -1,40 +1,41 @@
-let fs = require('fs')
+let fs = require("fs");
 function readFile(filename) {
-  return new Promise(function (resolve, reject) {
-    fs.readFile(filename, function (err, data) {
-      if (err)
-        reject(err)
-      else
-        resolve(data)
-    })
-  })
+  return new Promise(function(resolve, reject) {
+    fs.readFile(filename, function(err, data) {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
 }
-function *read() {
-  console.log(2)
-  let template = yield readFile('./template.txt')
-  console.log(3)
-  let data = yield readFile('./data.txt')
-  return template + '+' + data
+function* read() {
+  console.log(2);
+  let template = yield readFile("./template.txt");
+  console.log(3);
+  let data = yield readFile("./data.txt");
+  return template + "+" + data;
 }
-co(read).then(function (data) {
-  console.log(data)
-}, function (err) {
-  console.log(err)
-})
+co(read).then(
+  function(data) {
+    console.log(data);
+  },
+  function(err) {
+    console.log(err);
+  }
+);
 
 function co(gen) {
-  let it = gen()
-  return new Promise(function (resolve, reject) {
-    !function next(lastVal) {
-      console.log(1)
-      let {value, done} = it.next(lastVal)
+  let it = gen();
+  return new Promise(function(resolve, reject) {
+    !(function next(lastVal) {
+      console.log(1);
+      let { value, done } = it.next(lastVal);
       if (done) {
-        console.log(5)
-        resolve(value)
+        console.log(5);
+        resolve(value);
       } else {
-        console.log(4)
-        value.then(next, reason => reject(reason))
+        console.log(4);
+        value.then(next, reason => reject(reason));
       }
-    }()
-  })
+    })();
+  });
 }
---
+++ vscode.md
@@ -0,0 +1,24 @@
+# vscode 配置
+
+## 使用 node 启动调试
+
+{
+"type": "node",
+"request": "launch",
+"name": "启动程序",
+"program": "E:/cli.js"
+}
+
+## 保存时自动格式化
+
+File->Preferences->User Settings
+{
+"editor.formatOnType": false,
+"editor.formatOnSave": false
+}
+
+## 快捷键
+
+- 列选择 `alt+shift+左键`
+- 多点编辑 按 `alt` 点击左键
+- 格式化 `Alt+shift+f`
