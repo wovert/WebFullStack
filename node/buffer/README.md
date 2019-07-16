9f83bbaa874b1643102c8a207f92a603599abb37
wovert <67668283@qq.com>
2019-07-16 10:12:48 +0800

    change

---
+++ node/buffer/README.md
@@ -0,0 +1,9 @@
+# buffer
+
+> 二进制数据流
+
+操作文件或网络数据时，起始操作的是二进制数据流
+
+- new Buffer(size 字节)
+- new Buffer(array)
+- new Buffer(string, [encoding])
---
+++ node/buffer/index.js
@@ -0,0 +1,29 @@
+const bf = Buffer.alloc(5);
+console.log(bf);
+bf[0] = 1;
+bf[1] = 2;
+bf[2] = 3;
+bf[3] = 4;
+bf[4] = 5;
+bf[5] = 6;
+console.log(bf);
+
+//////////////////////////////////
+
+const arr = new Uint16Array(2);
+
+arr[0] = 5000;
+arr[1] = 4000;
+
+// 与 `arr` 共享内存。
+const buf = Buffer.from(arr.buffer);
+
+console.log(buf);
+// 打印: <Buffer 88 13 a0 0f>
+
+// 改变原先的 Uint16Array 也会改变 Buffer。
+arr[1] = 6000;
+
+console.log(buf);
+console.log(arr);
+// 打印: <Buffer 88 13 70 17>
